d5ef2853e6ad2ee4d9bb714e0bf8c5817eebda97
BHDHakim <hakim.bhd@gmail.com>
2022-06-09 15:52:15 +0300

    Refactore

---
+++ src/modules/todoStore.js
@@ -0,0 +1,49 @@
+class todoStore {
+  STORE_KEY = 'todo-list';
+
+  items = [];
+
+  constructor() {
+    this.items = this.fetchItems();
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.store(this.items);
+  }
+
+  edit(idx, newTodo) {
+    const index = this.items.findIndex(({ index }) => index === idx);
+
+    this.items[index].description = newTodo;
+    this.store(this.items);
+  }
+
+  delete(idx) {
+    const deleteIndex = this.items.findIndex(({ index }) => index === idx);
+
+    if (deleteIndex > -1) this.items.splice(deleteIndex, 1);
+    this.store(this.items);
+  }
+
+  updateStatus(idx, checked) {
+    const index = this.items.findIndex(({ index }) => index === idx);
+
+    this.items[index].completed = checked;
+    this.store(this.items);
+  }
+
+  store(items) {
+    const data = JSON.stringify(items);
+
+    localStorage.setItem(this.STORE_KEY, data);
+  }
+
+  fetchItems() {
+    const items = localStorage.getItem(this.STORE_KEY);
+
+    return items ? JSON.parse(items) : [];
+  }
+}
+
+export default todoStore;
